13af59ef5c37b783bae49ddde760bbb1b17f87a3
Eyuleo <eyuleo10@gmail.com>
2024-08-12 17:42:53 +0300

    restructure the project

--- models/note.js
+++ models/note.js
@@ -1,26 +1,10 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose
-	.connect(url)
-	.then(() => {
-		console.log('connected to DB')
-	})
-	.catch((error) => {
-		console.log('error connecting to DB', error.message)
-	})
 
 const noteSchema = new mongoose.Schema({
 	content: {
 		type: String,
-		minLength: 5,
 		required: true,
+		minlength: 5,
 	},
 	important: Boolean,
 })
